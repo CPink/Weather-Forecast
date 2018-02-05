d74863a4aa3f042bc6df3e212ae401bc36160b92
Chance Pinkerton <C_pink@Chances-MacBook-Pro.local>
2018-02-05 03:04:48 -0700

    added api and api call to get temp based on city name

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-(function(){
-
-'use strict';
 
 var bbWeatherApp = angular.module('bbWeatherApp', ['ngRoute', 'ngResource']);
 
@@ -20,24 +17,40 @@ bbWeatherApp.config( function($routeProvider) {
 
 
 bbWeatherApp.service('cityService', function(){
-    this.city = "New York, NY"
+    this.city = "New York"
 })
 
 //controllers
 bbWeatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
 
     $scope.city = cityService.city;
-    $scope.watch('city', function(){
+
+    $scope.$watch('city', function(){
         cityService.city = $scope.city;
     });
     
 }]);
      
 
-bbWeatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService){
+bbWeatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService ){
     
     $scope.city = cityService.city;
 
+    $scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/weather?APPID=1a30405027e056950d37eeaa11bf7ebe", {
+        callback: 'JSON_CALLBACK'}, { get: { method: "JSONP"}});
+
+
+        $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city });
+        console.log($scope.weatherResult);
+        
+        $scope.convertToFahrenheit = function(degk){
+            return Math.round((1.8 * (degk - 273)) + 32);
+
+            $scope.convertTotDate = function(dt){
+                return new Date(dt * 1000);
+            }
+        }
+
 }]);
 
-}());
+//Directives
